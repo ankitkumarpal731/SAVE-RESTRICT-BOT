3c6f834d352cc46925e20fa4ade46876d4f34211
ankitkumarpal731 <96754831+ankitkumarpal731@users.noreply.github.com>
2025-12-31 12:11:44 +0530

    Change LOG_CHANNEL to read from environment variable

--- config.py
+++ config.py
@@ -12,7 +12,7 @@ API_HASH = os.environ.get("API_HASH", "df4f77699cd2fc8ef462af9bb460f5ad")
 ADMINS = [int(id) for id in os.environ.get("ADMINS", "8496861897").split(",")]
 DB_URI = os.environ.get("DB_URI", "mongodb+srv://ankitkumarpal731_db_user:Ankit731@cluster0.evdpt8j.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "REXSaveRestricted")
-LOG_CHANNEL = 0
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "0"))
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
 # Rexbots
