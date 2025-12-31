78b3821ac5c51b04118abd23f488fb5289e0ef14
ankitkumarpal731 <96754831+ankitkumarpal731@users.noreply.github.com>
2025-12-31 10:59:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,13 +6,13 @@
 # Don't Remove Credit 
 # Telegram Channel @RexBots_Official
 import os
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-API_ID = int(os.environ.get("API_ID", ""))
-API_HASH = os.environ.get("API_HASH", "")
-ADMINS = [int(id) for id in os.environ.get("ADMINS", "").split(",")]
-DB_URI = os.environ.get("DB_URI", "")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8380636849:AAEz89QD5cbDgwXV7YwpY2xaUMXdS-khhRE")
+API_ID = int(os.environ.get("API_ID", "32416483"))
+API_HASH = os.environ.get("API_HASH", "df4f77699cd2fc8ef462af9bb460f5ad")
+ADMINS = [int(id) for id in os.environ.get("ADMINS", "8496861897").split(",")]
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://ankitkumarpal731_db_user:Ankit731@cluster0.evdpt8j.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "REXSaveRestricted")
-LOG_CHANNEL = 
+LOG_CHANNEL = 0
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
 # Rexbots
